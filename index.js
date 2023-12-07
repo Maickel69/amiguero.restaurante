d46483254b66bf12f52ffe496fa32c6ca82ecba9
Maickel69 <8040182@unamad.edu.pe>
2023-12-07 16:46:37 -0500

    añadir conf al server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const cors = require("cors")
+const app= express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(3000,(err)=>{
+    if(err) console.log("un error manooo")
+    console.log("server coriendo papu")
+
+} )
